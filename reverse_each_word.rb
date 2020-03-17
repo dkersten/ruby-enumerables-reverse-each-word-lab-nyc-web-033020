072625d1bd7be6dbc4ce088f10c5551b21bfa83e
Daniel Kersten <dkersten1994@gmail.com>
2020-03-17 11:22:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,3 +8,14 @@ def reverse_each_word(string)
   puts reversed_array.join(" ")
   reversed_array.join(" ")
 end
+
+def reverse_each_word(string)
+  array = string.split(" ")
+  reversed_array = []
+  
+  array.collect do |i|
+    reversed_array << i.reverse
+  end
+  puts reversed_array.join(" ")
+  reversed_array.join(" ")
+end
