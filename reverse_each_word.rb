79598982c2c5c32a9536b34efed26749082b0160
Daniel Kersten <dkersten1994@gmail.com>
2020-03-17 11:21:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(string)
   array = string.split(" ")
-  reversed_array = array.each do |i|
+  reversed_array = []
+  
+  array.each do |i|
     reversed_array << i.reverse
   end
-  reversed_array.join(" ")
 end
